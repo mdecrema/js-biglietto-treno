e884bc7e7ad075089896bda63a5e93b5bc0ec156
marco <marcodecrema@libero.it>
2020-08-04 17:06:35 +0200

    calcolo costo biglietto meno lo sconto

--- js/script.js
+++ js/script.js
@@ -4,12 +4,17 @@ var distanza = parseInt(prompt('Quanti Km devi percorrere?'));
 var età = parseInt(prompt('Quanti anni hai?'));
 // Calcolo costo del biglietto in base alla tariffa di 0.21€/Km
 var prezzoBiglietto = distanza * 0.21;
-// Eventuale sconto per minorenni e over-65
+// Eventuale sconto per minorenni
+var scontoMinorenni = ((prezzoBiglietto*20)/100).toFixed(2);
+// Eventuale sconto per over-65
+var scontoOver65 = ((prezzoBiglietto*40)/100).toFixed(2);
+
+// Calcolo Totale
 if (età < 18) {
-  var prezzoBigliettoScontato = ((prezzoBiglietto*20)/100).toFixed(2);
+  var prezzoBigliettoScontato = prezzoBiglietto - scontoMinorenni;
   document.getElementById('totale').innerHTML="Il costo del suo biglietto è: €"+prezzoBigliettoScontato;
 } else if (età >= 65) {
-  var prezzoBigliettoScontato = ((prezzoBiglietto*40)/100).toFixed(2);
+  var prezzoBigliettoScontato = prezzoBiglietto - scontoOver65;
   document.getElementById('totale').innerHTML="Il costo del suo biglietto è: €"+prezzoBigliettoScontato;
 } else {
   document.getElementById('totale').innerHTML="Il costo del suo biglietto è: €"+prezzoBiglietto;
